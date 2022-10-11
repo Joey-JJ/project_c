cb85764ed1fbd74c0e58b0fe58672c390b80f715
Joey van der Valk <joeyvandervalk@gmail.com>
2022-10-11 09:48:30 +0200

    Installed TailwindCSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,9 @@
 import type { NextPage } from "next";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div className={"container"}>
       <h1>Dashboard Cavero</h1>
       <Image
         src={require("./../public/logo_loods.png")}
