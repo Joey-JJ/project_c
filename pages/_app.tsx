d3a5d79e74c86de7ad10e1ba9ce36eca6b02995b
Joey van der Valk <joeyvandervalk@gmail.com>
2022-10-11 10:14:02 +0200

    Set up supabase session context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,52 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import React, { useState, useEffect } from "react";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import { supabase } from "../utils/supabaseClient";
+import { sessionContext } from "../context/sessionContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [isLoading, setIsLoading] = useState(true);
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    let mounted = true;
+
+    const fetchSession = async () => {
+      const {
+        data: { session: any },
+      } = await supabase.auth.getSession();
+
+      if (mounted) {
+        if (session) {
+          setSession(session);
+        }
+
+        setIsLoading(false);
+      }
+    };
+
+    fetchSession();
+
+    const { subscription }: any = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session as any);
+      }
+    );
+
+    return () => {
+      mounted = false;
+      subscription?.unsubscribe();
+    };
+  }, []);
+
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <sessionContext.Provider value={session}>
+      <Component {...pageProps} />
+    </sessionContext.Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
