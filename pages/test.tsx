535b0dea41ddf4865e04ed3587f8fc6009bde5f2
Mees2004 <meesverheul04@gmail.com>
2022-11-14 09:41:53 +0100

    Create test.tsx

---
+++ pages/test.tsx
@@ -0,0 +1,176 @@
+import type { NextPage } from "next";
+
+import React from "react";
+const Dashboard: NextPage = () => {
+    return(
+<div className="overflow-x-auto">
+  <table className="table w-full">
+    
+    <thead>
+      <tr>
+        <th>Stations</th>
+      </tr>
+    </thead>
+    <tbody>
+    <div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  <tr className="hover">
+        <th>1</th>
+        <td>Station 1</td>
+        
+      </tr>
+  </div>
+  <div className="collapse-content"> 
+    <p><tr>
+    <thead>
+      <tr>
+        <th>Status</th>    
+        <th>Kenteken</th>
+    </tr>
+    </thead>  
+        <tr>
+        <td>Vrij</td>
+        <td>*********</td>
+    </tr>
+      </tr></p>
+  </div>
+</div>
+      
+      
+<div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  <tr className="hover">
+        <th>2</th>
+        <td>Station 2</td>
+        
+      </tr>
+  </div>
+  <div className="collapse-content"> 
+    <p><tr>
+    <thead>
+      <tr>
+        <th>Status</th>    
+        <th>Kenteken</th>
+    </tr>
+    </thead>  
+        <tr>
+        <td>Vrij</td>
+        <td>*********</td>
+    </tr>
+      </tr></p>
+  </div>
+</div>
+      
+<div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  <tr className="hover">
+        <th>3</th>
+        <td>Station 3</td>
+        
+      </tr>
+  </div>
+  <div className="collapse-content"> 
+    <p><tr>
+    <thead>
+      <tr>
+        <th>Status</th>    
+        <th>Kenteken</th>
+    </tr>
+    </thead>  
+        <tr>
+        <td>Vrij</td>
+        <td>*********</td>
+    </tr>
+      </tr></p>
+  </div>
+</div>
+
+<div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  <tr className="hover">
+        <th>4</th>
+        <td>Station 4</td>
+        
+      </tr>
+  </div>
+  <div className="collapse-content"> 
+    <p><tr>
+    <thead>
+      <tr>
+        <th>Status</th>    
+        <th>Kenteken</th>
+    </tr>
+    </thead>  
+        <tr>
+        <td>Vrij</td>
+        <td>*********</td>
+    </tr>
+      </tr></p>
+  </div>
+</div>
+<div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  <tr className="hover">
+        <th>5</th>
+        <td>Turbo station 1</td>
+        
+      </tr>
+  </div>
+  <div className="collapse-content"> 
+    <p><tr>
+    <thead>
+      <tr>
+        <th>Status</th>    
+        <th>Kenteken</th>
+    </tr>
+    </thead>  
+        <tr>
+        <td>Vrij</td>
+        <td>*********</td>
+    </tr>
+      </tr></p>
+  </div>
+</div>
+<div tabIndex={0} className="collapse collapse-arrow border border-base-300 bg-base-100 rounded-box">
+  <div className="collapse-title text-xl font-medium">
+  <tr className="hover">
+        <th>6</th>
+        <td>Turbo station 2</td>
+        
+      </tr>
+  </div>
+  <div className="collapse-content"> 
+    <p><tr>
+    <thead>
+      <tr>
+        <th>Status</th>    
+        <th>Kenteken</th>
+    </tr>
+    </thead>  
+        <tr>
+        <td>Vrij</td>
+        <td>*********</td>
+    </tr>
+      </tr></p>
+  </div>
+</div>
+    </tbody>
+  </table>
+</div>
+  
+
+
+
+    );
+};
+
+export default Dashboard;   
+
+  
+
+
+
+
+
+
+
