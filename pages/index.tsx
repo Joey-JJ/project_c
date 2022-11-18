ad2cc23daf661718aed9dafec9a48b8c6bc9cd0f
Saronny <101633650+Saronny@users.noreply.github.com>
2022-11-18 03:57:15 +0100

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import TicketCount from "../components/TicketCount"
 
 import { supabase } from "../utils/supabaseClient";
 import { sessionContext } from "../context/sessionContext";
+import AccountInfo from "./AccountInfo";
 
 const Home: NextPage = () => {
   const { session }: any = useContext(sessionContext);
@@ -18,8 +19,7 @@ const Home: NextPage = () => {
         width={300}
         height={200} />
       <p>Logged in as: {session.user.email}</p>
-      <Register/> 
-      <TicketCount></TicketCount>
+      <AccountInfo/>
       <button
         onClick={() => supabase.auth.signOut()}
         className="btn btn-primary"
