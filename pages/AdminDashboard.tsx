b87d1c784f39562f8ac54215a65a69e6f349b4b7
Mike <1026366@hr.nl>
2022-11-28 01:53:35 +0100

    worked on AdminDashboard.tsx added chart react     -modified package-lock.json     -modified package.json created chart.tsx     - standard prototype chart

--- pages/AdminDashboard.tsx
+++ pages/AdminDashboard.tsx
@@ -1,11 +1,12 @@
 import React, { FC, useEffect, useState } from "react";
+import Chart from "../components/Chart";
 
 //make 6 different charging stations with a status
 
 const STATUS_KEY = {
-    BEING_USED: "being_used",
-    ALMOST_OVER: "almost_over",
-    FREE: "free",
+  BEING_USED: "Being_used",
+  ALMOST_OVER: "Almost_over",
+  FREE: "Free",
 };
 
 const STATUS_COLOR = {
@@ -24,47 +25,119 @@ interface ChargeStationProps {
   status: string;
 }
 
-const badgeColor = (status: string) => {
-    switch (status) {
-        case STATUS_KEY.BEING_USED:
-            return <div>
-                <span className="badge-error">{status}</span>
-            </div>
-        case STATUS_KEY.ALMOST_OVER:
-            return <div>
-                <span className="badge-warning">{status}</span>
-            </div>
-        case STATUS_KEY.FREE:
-            return <div>
-                <span className="badge-success">{status}</span>
-            </div>
-        default:
-            return <div>
-                <span className="badge-success">{status}</span>
-            </div>
-    }
+
+const USER = {
+  NAME : "John Doe",
+  L_NUMBER : "G-001-BB",
+  EMAIL : "JohnDoe@test.nl",
+}
+
+const RemoveUnderScore = (name: string) => {
+  return name.replace(/_/g, " ");
 };
+
+const isUsed = (status: string) => {
+  switch (status) {
+    case STATUS_KEY.BEING_USED:
+      return (
+        <div>
+          <p>Current user: {USER.NAME}</p>
+          <p>License plate number: {USER.L_NUMBER}</p>
+          <p>Email: {USER.EMAIL}</p>
+        </div>
+      );
+    case STATUS_KEY.ALMOST_OVER:
+      return (
+        <div>
+          <p>Current user: {USER.NAME}</p>
+          <p>License plate number: {USER.L_NUMBER}</p>
+          <p>Email: {USER.EMAIL}</p>
+        </div>
+      );
+    case STATUS_KEY.FREE:
+      return (
+        <div>
+          <p>Current user: None</p>
+          <p>License plate number: None</p>
+          <p>Email: None</p>
+        </div>
+    );
+    default:
+      return(
+        <div>
+          <p>Current user: None</p>
+          <p>License plate number: None</p>
+          <p>Email: None</p>
+        </div>
+      );
+  }
+}
+
+
+const badgeColor = (status: string) => {
+  switch (status) {
+    case STATUS_KEY.BEING_USED:
+      return (
+        <div>
+          <span className="badge badge-error">{RemoveUnderScore(status)}</span>
+        </div>
+      );
+    case STATUS_KEY.ALMOST_OVER:
+      return (
+        <div>
+          <span className="badge badge-warning">
+            {RemoveUnderScore(status)}
+          </span>
+        </div>
+      );
+    case STATUS_KEY.FREE:
+      return (
+        <div>
+          <span className="badge badge-success">
+            {RemoveUnderScore(status)}
+          </span>
+        </div>
+      );
+    default:
+      return (
+        <div>
+          <span className="badge badge-success">
+            {RemoveUnderScore(status)}
+          </span>
+        </div>
+      );
+  }
+};
+
+
+
+
 
 const ChargeStation: FC<ChargeStationProps> = ({ name, status }) => {
   return (
-    <div className="card w-96 bg-base-100 shadow-xl">
+    <div className="card w-96 bg-base-100 card-bordered shadow-xl overflow-visible">
       <div className="card-body">
         <h2 className="card-title">{name}</h2>
-        <p>
-            {badgeColor(status)}
-        </p>
+        <p>{badgeColor(status)}</p>
         <div className="card-actions justify-end">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn m-1">
-              Click
+          <div className="dropdown dropdown-hover overflow-visible">
+            <label tabIndex={0} className="btn btn-sm">
+              More
             </label>
             <div
               tabIndex={0}
-              className="dropdown-content card card-compact w-64 p-2 shadow bg-primary text-primary-content"
+              className="dropdown-content card card-bordered shadow-xl w-auto h-auto p-2  bg-base-100  text-black"
             >
               <div className="card-body">
-                <h3 className="card-title">Stats!</h3>
-                <p>Some stats about this item.</p>
+                <h3 className="card-title">Info</h3>
+                <h4>Usage: </h4>
+                <p>
+                  {isUsed(status)}
+                </p>
+              <h4>Stats: </h4>
+                <p>
+                  <Chart />
+                </p>
               </div>
             </div>
           </div>
@@ -76,42 +149,42 @@ const ChargeStation: FC<ChargeStationProps> = ({ name, status }) => {
 
 const AdminDashboard: FC = () => {
   //making a admin dashboard
-    const [chargeStationData, setChargeStationData] = useState<ChargeType[]>([]);
+  const [chargeStationData, setChargeStationData] = useState<ChargeType[]>([]);
 
-    useEffect(() => {
-        getFakeData();
-        setInterval(getFakeData, 3000);
-    }, []);
+  useEffect(() => {
+    getFakeData();
+    setInterval(getFakeData, 3000);
+  }, []);
 
-    const getFakeData = () => {
-        const statusKeys = Object.values(STATUS_KEY);
-        const updatedFakeData = [];
-        for (let i = 0; i < 6; i++) {
-            updatedFakeData.push({
-                name: i < 4 ? `Laadpaal ${i + 1}` : `Fast charge ${i - 3}`,
-                status: statusKeys[Math.floor(Math.random() * statusKeys.length)],
-            });
-        }
-        setChargeStationData(updatedFakeData);
-    };
-
-    if (!chargeStationData?.length) {
-        return null;
+  const getFakeData = () => {
+    const statusKeys = Object.values(STATUS_KEY);
+    const updatedFakeData = [];
+    for (let i = 0; i < 6; i++) {
+      updatedFakeData.push({
+        name: i < 4 ? `Laadpaal ${i + 1}` : `Fast charge ${i - 3}`,
+        status: statusKeys[Math.floor(Math.random() * statusKeys.length)],
+      });
     }
+    setChargeStationData(updatedFakeData);
+  };
+
+  if (!chargeStationData?.length) {
+    return null;
+  }
 
   return (
     <div>
-      <div className="container mx-auto min-h-screen flex flex-col">
+      <div className="container mx-auto min-h-screen flex flex-col items-center overflow-visible">
         <div className="grid grid-cols-2 gap-4">
-      {chargeStationData.map((chargeStation, idx) => (
-          <ChargeStation
-            key={idx}
-            name={chargeStation.name}
-            status={chargeStation.status}
-          />
-        ))}
+          {chargeStationData.map((chargeStation, idx) => (
+            <ChargeStation
+              key={idx}
+              name={chargeStation.name}
+              status={chargeStation.status}
+            />
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 };
