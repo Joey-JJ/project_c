6225304af4908291aabd9706ba0147f93d9dd377
Saronny <101633650+Saronny@users.noreply.github.com>
2022-12-02 15:07:53 +0100

    layout fixen

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,42 +1,17 @@
 import { useContext } from "react";
 import type { NextPage } from "next";
-import Image from "next/image";
-import TicketCount from "../components/TicketCount";
-import Notification from "../components/Notification";
 import { sessionContext } from "../context/sessionContext";
-import LuutChargeStations from "../components/LuutChargeStations";
+import VisitorDashboard from "../components/VisitorDashboard";
+import AdminDashboard from "../components/AdminDashboard";
+
 
 const Home: NextPage = () => {
   const { session }: any = useContext(sessionContext);
+  const isAdmin = session.user.email.ToLower() === "caverobeheerder@gmail.com" ? true : false;
+
 
   return (
-    <div className="">
-      <div className="container mx-auto min-h-[calc(100vh-70px)] flex flex-col items-center gap-10  ">
-        <p>Logged in as: {session.user.email}</p>
-        <label htmlFor="my-modal-4" className="btn btn-primary">
-          Instructions
-        </label>
-
-        <LuutChargeStations />
-
-        <div className="flex">
-          <Notification />
-          <TicketCount />
-        </div>
-      </div>
-      <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-      <label htmlFor="my-modal-4" className="modal cursor-pointer">
-        <label className="modal-box relative" htmlFor="">
-          <h3 className="text-lg font-bold">How it works</h3>
-          <p className="py-4">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
-            consectetur quae dolores provident modi nobis ex quia illum
-            dignissimos, dicta accusantium deleniti minus soluta voluptas
-            maiores nesciunt aspernatur architecto obcaecati!
-          </p>
-        </label>
-      </label>
-    </div>
+  (isAdmin ? <AdminDashboard /> : <VisitorDashboard />)
   );
 };
 
