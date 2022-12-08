b386f26f4ec5a1b2fbed2e0689c8806b8ca964b1
yourickb <1032606@hr.nl>
2022-12-08 22:18:52 +0100

    Raffle geupdate met werkende submit

--- components/TicketCountAdmin.tsx
+++ components/TicketCountAdmin.tsx
@@ -1,29 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { arrayBuffer } from "stream/consumers";
+var numberoftickets = 0;
+var emails = "";
+
+
 
 const TicketCountAdmin = () => {
+    const [raffledtickets, setRaffledtickets] = useState("");
+    
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        {parseInt(raffledtickets) > 0 ? numberoftickets = numberoftickets + parseInt(raffledtickets) : alert(`Please enter a proper number`)}
+        {parseInt(raffledtickets) > 0 ? alert(`You just raffled! Click on see results to see the results of this raffle.`): emails = emails}
+        for (let i = 0; i < parseInt(raffledtickets); i++){
+            emails = emails + "testemail@email.com "}
+        
+        
+    }
+    
     return(
         <div className="stats w-70 bg-base-100 card-bordered shadow-xl overflow-visible p-2 md:p-1 lg:p-0">
             <div className="">
             <div className="stat p-2 md:p-2 lg:p-8">
                 <div className="stat-title text-center lg:text-left">Total tickets raffled off</div>
-                <div className="stat-value text-center lg:text-left">14</div>
+                <div className="stat-value text-center lg:text-left">{numberoftickets}</div>
                 &nbsp;
-                <input type="text" placeholder="Input amount of tickets to raffle off..." className="input input-bordered w-full max-w-xs text-center lg:text-left mb-2" />
                 <div className="flex flex-col space-y-0.5 hover:space justify-center items-center">
-                <label htmlFor="my-modal-4" className="btn btn-wide lg:w-80">
-                Raffle tickets
-                </label>
-                </div>
-                <input type="checkbox" id="my-modal-4" className="modal-toggle" />
-                <label htmlFor="my-modal-4" className="modal cursor-pointer">
-                    <label className="modal-box relative" htmlFor="">
-                    <h3 className="text-lg font-bold text-left">The winners of this raffle</h3>
-                    <div className="whitespace-pre-line">
-                            kaassoufle@email.com, salade@email.com, kaassouflesalademetsausenei@email.com, k@email.com and meertesting@email.com!
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Input amount of tickets to raffle off..." value={raffledtickets} onChange={(e) => setRaffledtickets(e.target.value)} className="input input-bordered w-full max-w-xs text-center lg:text-left mb-2" />
+                    <div>
+                        
+                        <input type="submit" value="Raffle Tickets" className="btn btn-wide lg:w-40 items-center"  
+                        />
+                        <label htmlFor="my-modal-4" className="btn btn-wide lg:w-40">
+                            See results
+                        </label>
+                        <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+                        <label htmlFor="my-modal-4" className="modal cursor-pointer">
+                            <label className="modal-box relative" htmlFor="">
+                                <h3 className="text-lg font-bold text-left">The winners of this raffle</h3>
+                            <div className="whitespace-pre-line">
+                                {emails}
+                            </div>
+                            </label>
+                        </label>
                     </div>
-                </label>
-                </label>
-            </div>
+                </form>
+                </div>
+                </div>
             </div>
         </div>
     );
