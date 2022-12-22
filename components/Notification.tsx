d847af506592aa7c20f8b2009dd81d274acad545
yourickb <1032606@hr.nl>
2022-12-22 16:13:39 +0100

    Bel werkend met database

--- components/Notification.tsx
+++ components/Notification.tsx
@@ -7,17 +7,7 @@ const Notification: FC = () => {
   const [marked, setMarked] = useState<boolean>(true);
   const [loading, setLoading] = useState(true);
   const { session } = useSessionContext();
-  /*const fetchBell = async () => {
-    const { data } = await supabase
-      .from('profiles')
-      .select('Notifications')
-      .eq("id", session?.user.id);
-    if (data) {
-      alert(data)
-      setMarked(!marked)
-    }
-}*/
-  
+  // fetcht de status en weergeeft het op een correcte manier op het scherm
   useEffect(() => {
     const fetchBell = async () => {
       const { data } = await supabase
@@ -26,34 +16,24 @@ const Notification: FC = () => {
         .eq("id", session?.user.id)
         .single();
       if (data) {
-        console.log(data)
-        //setMarked(data as any)
-        setMarked(false);
-        data ? setMarked(true) : setMarked(false);
+        setMarked(data.Notifications)
       }
     }
   fetchBell();
   }, []);
-
-  useEffect(() => {
-// fetch bool and flip and return to db
-  }, [marked])
-
-  
-
-
-  
-  
-  
-  
-  
-  
-
-
+  // updateBell wordt aangeroepen bij de druk op de knop
+  async function updateBell () {
+    const { data } = await supabase
+      .from('profiles')
+      .update({Notifications: !marked})
+      .eq('Notifications', marked)
+      .eq("id", session?.user.id)
+      
+  }
   return (
     <div className="max-w-[15rem] flex flex-col justify-center items-center gap-2 stats shadow py-2">
-      <div className="max-w-max" onClick={() =>  setMarked(prev => !prev)}>
-        {marked ? <BsBell size={50} /> : <BsBellSlash size={50} />}
+      <div className="max-w-max" onClick={() => { updateBell(); setMarked(!marked);}}> 
+        {marked ? <BsBell size={50} /> : <BsBellSlash size={50} /> /*  veranderd de bel in realtime */}
       </div>
       <p className="text-xs text-center">
         Click the bell to be notified when a charging spot becomes available
