78f427e9a61d47ee214bd56aa0a47d2a32855805
Joey <joeyvandervalk@gmail.com>
2022-12-31 00:09:48 +0100

    Stopping charging session hooked up to db

--- components/ChargeStation.tsx
+++ components/ChargeStation.tsx
@@ -1,48 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import type { ChargeStationType } from "../Types/ChargeStationType";
 import { supabase } from "../utils/supabaseClient";
 import { useSessionContext } from "../context/sessionContext";
 
 const ChargeStation: React.FC<{
   station: ChargeStationType;
-  currentlyCharging: boolean;
-}> = ({ station, currentlyCharging }) => {
+  setCurrentlyCharging: React.Dispatch<React.SetStateAction<boolean>>;
+}> = ({ station, setCurrentlyCharging }) => {
   const { session } = useSessionContext();
+  const [taken, setTaken] = useState<boolean>(station.is_taken);
 
   const startChargingHandler = async () => {
-    const { error } = await supabase.from("charging_sessions").insert([
-      {
-        started_at: new Date().toISOString(),
-        taken_by: session?.user.id,
-        charger_id: station.id,
-      },
-    ]);
+    try {
+      const { error } = await supabase.from("charging_sessions").insert([
+        {
+          started_at: new Date().toISOString(),
+          taken_by: session?.user.id,
+          charger_id: station.id,
+        },
+      ]);
 
-    if (error) {
+      if (error) {
+        throw error;
+      }
+
+      const { error: error2 } = await supabase
+        .from("charging_stations")
+        .update({ is_taken: true, taken_by: session?.user.id })
+        .eq("id", station.id);
+
+      if (error2) {
+        throw error2;
+      }
+
+      setTaken(true);
+      setCurrentlyCharging(true);
+    } catch (error: any) {
       alert(error.message);
-    }
-
-    const { error: error2 } = await supabase
-      .from("charging_stations")
-      .update({ is_taken: true })
-      .eq("id", station.id);
-
-    if (error2) {
-      alert(error2.message);
     }
   };
 
   return (
-    <div className="p-4 border-2 flex flex-col gap-2 justify-between items-center">
+    <div
+      className={`p-4 border-2 ${
+        taken && "border-error bg-red-200"
+      } flex flex-col gap-2 justify-between items-center`}
+    >
       <h1>
         {!station.is_supercharger ? "Charger" : "Supercharger"} - {station.id}
       </h1>
       <button
         onClick={startChargingHandler}
-        className="btn-primary btn-xs"
-        disabled={station.is_taken}
+        className={`${!taken ? "btn-primary" : "btn-error"} btn-xs`}
+        disabled={taken}
       >
-        Start charging
+        {taken ? "Taken" : "Start charging"}
       </button>
     </div>
   );
--- components/ChargeStations.tsx
+++ components/ChargeStations.tsx
@@ -2,14 +2,46 @@ import React, { useEffect, useState } from "react";
 import { supabase } from "../utils/supabaseClient";
 import ChargeStation from "./ChargeStation";
 import type { ChargeStationType } from "../Types/ChargeStationType";
+import { useSessionContext } from "../context/sessionContext";
 
 const ChargeStations: React.FC = () => {
+  const { session } = useSessionContext();
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<boolean>(false);
   const [currentlyCharging, setCurrentlyCharging] = useState<boolean>(false);
   const [chargingStations, setChargingStations] = useState<ChargeStationType[]>(
     []
   );
+
+  const stopChargingHandler = async () => {
+    try {
+      //update charging session
+      const { error } = await supabase
+        .from("charging_sessions")
+        .update({ ended_at: new Date().toISOString() })
+        .eq("taken_by", session?.user.id)
+        .order("started_at", { ascending: false })
+        .limit(1);
+
+      if (error) {
+        throw error;
+      }
+
+      //update charging station
+      const { error: error2 } = await supabase
+        .from("charging_stations")
+        .update({ is_taken: false, taken_by: null })
+        .eq("taken_by", session?.user.id);
+
+      if (error2) {
+        throw error2;
+      }
+
+      setCurrentlyCharging(false);
+    } catch (error: any) {
+      alert(error.message);
+    }
+  };
 
   useEffect(() => {
     const fetchChargeStations = async () => {
@@ -48,10 +80,26 @@ const ChargeStations: React.FC = () => {
 
   return (
     <>
-      <h1>
-        You are currently not charging, start a session by choosing a free
-        charger.
-      </h1>
+      {!currentlyCharging && (
+        <h1>
+          You are currently not charging, start a session by choosing a free
+          charger.
+        </h1>
+      )}
+      {currentlyCharging && (
+        <div className="border-2 flex flex-col gap-4 p-4 items-center justify-center">
+          <h1>
+            You are currently charging, please stop the session when you are
+            done.
+          </h1>
+          <button
+            onClick={stopChargingHandler}
+            className="btn btn-warning max-w-xs"
+          >
+            Stop charging
+          </button>
+        </div>
+      )}
       <div className="grid grid-cols-2 gap-2">
         {chargingStations
           .sort((a, b) => a.id - b.id)
@@ -59,7 +107,7 @@ const ChargeStations: React.FC = () => {
             <ChargeStation
               key={station.id}
               station={station}
-              currentlyCharging={currentlyCharging}
+              setCurrentlyCharging={setCurrentlyCharging}
             />
           ))}
       </div>
