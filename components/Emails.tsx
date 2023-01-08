d43a5a2dbc988fbc0c767f9200b95633c61ad1b1
Joey <joeyvandervalk@gmail.com>
2023-01-08 12:32:02 +0100

    Added error handling to emails

--- components/Emails.tsx
+++ components/Emails.tsx
@@ -1,18 +1,33 @@
 import React, { useState, useEffect } from "react";
 
 const Emails: React.FC = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const [emails, setEmails] = useState([]);
 
   useEffect(() => {
-    fetch("api/getEmails")
-      .then((res) => res.json())
-      .then((data) => setEmails(data))
-      .catch((err) => console.log(err));
+    try {
+      fetch("api/getEmails")
+        .then((res) => res.json())
+        .then((data) => setEmails(data))
+        .catch((err) => {
+          throw new Error(err);
+        });
+    } catch (err: any) {
+      setError(true);
+      alert(err.message);
+    } finally {
+      setLoading(false);
+    }
   }, []);
+
   let i = 0;
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error, could not fetch emails</p>;
+
   return (
-    <div className="max-w-xl overflow-scroll my-10">
+    <div className="max-w-xl overflow-scroll my-10 max-h-80">
       <table className="table table-compact">
         <thead>
           <tr>
